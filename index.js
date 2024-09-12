4d7eac5ac454d8d15617bcc34378f3ee0c75033e
unknown <vigneshgoud12@gmail.com>
2024-09-12 11:15:49 +0530

    added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 1st question
 let arry =[2,7,8,8,]
 emp =[],uni=" ",dup =" "
 for(i of arry){
@@ -16,3 +17,34 @@ for(i of emp){
     }
    console.log(i+"->"+c);
 }
+
+
+
+// 2nd question
+
+
+let array =[1,2,1,4]
+emp =[],uni=" ",dup =" "
+for(i of array){
+    if(!emp.includes(i)){
+        emp.push(i)
+    }
+}
+
+for(i of emp){
+    c=0
+    for(j of array){
+        if(i==j){
+            c++
+        }
+        
+    }
+    if(c==1){
+        uni=uni+i+" "
+    }
+    else{
+        dup+=i+" "
+    }
+}
+console.log("uniques are:"+uni)
+console.log("duplicates are:"+dup)
